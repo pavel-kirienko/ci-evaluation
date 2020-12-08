5cb82d95a18c2d25c41b6a0c4242714dc0283423
Pavel Kirienko <pavel.kirienko@gmail.com>
2020-12-08 18:27:53 +0200

    Try a cleaner approach

--- test.py
+++ test.py
@@ -5,10 +5,9 @@ import sys
 import pathlib
 import importlib.util
 
-dll_path = pathlib.Path(importlib.util.find_spec('libpcap').origin).parent / '_platform' / '_windows' / ('x64' if sys.maxsize.bit_length() > 32 else 'x86') / 'wpcap'
-print('dll_path', dll_path)
-os.environ['PATH'] += os.pathsep + str(dll_path)
+if sys.platform.startswith('win'):
+    libpcap_dir = pathlib.Path(importlib.util.find_spec('libpcap').origin).parent
+    dll_path = libpcap_dir / '_platform' / '_windows' / ('x64' if sys.maxsize.bit_length() > 32 else 'x86') / 'wpcap'
+    os.environ['PATH'] += os.pathsep + str(dll_path)
 
 import libpcap
-
-
