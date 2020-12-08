481029c0017c5ea2b369cdf046d0d72caa9f99d8
Pavel Kirienko <pavel.kirienko@gmail.com>
2020-12-08 18:15:35 +0200

    'util' has to be imported explicitly on Windows

--- test.py
+++ test.py
@@ -3,11 +3,11 @@
 import os
 import sys
 import pathlib
-import importlib
+import importlib.util
 
 dll_path = pathlib.Path(importlib.util.find_spec('libpcap').origin).parent / '_platform' / '_windows' / ('x64' if sys.maxsize.bit_length() > 32 else 'x86')
 print('dll_path', dll_path)
-os.environ['PATH'] += os.pathsep + dll_path
+os.environ['PATH'] += os.pathsep + str(dll_path)
 
 import libpcap
 
