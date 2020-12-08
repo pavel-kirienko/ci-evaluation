7e6dd02673edbd0e4fc3d17622147e9ab13be8a5
Pavel Kirienko <pavel.kirienko@gmail.com>
2020-12-08 18:36:02 +0200

    Make it optional

--- test.py
+++ test.py
@@ -6,8 +6,11 @@ import pathlib
 import importlib.util
 
 if sys.platform.startswith('win'):
-    libpcap_dir = pathlib.Path(importlib.util.find_spec('libpcap').origin).parent
-    dll_path = libpcap_dir / '_platform' / '_windows' / ('x64' if sys.maxsize.bit_length() > 32 else 'x86') / 'wpcap'
-    os.environ['PATH'] += os.pathsep + str(dll_path)
+    spec = importlib.util.find_spec('libpcap')
+    if spec:
+        is_64_bit = sys.maxsize.bit_length() > 32
+        libpcap_dir = pathlib.Path(spec.origin).parent
+        dll_path = libpcap_dir / '_platform' / '_windows' / ('x64' if is_64_bit else 'x86') / 'wpcap'
+        os.environ['PATH'] += os.pathsep + str(dll_path)
 
 import libpcap
