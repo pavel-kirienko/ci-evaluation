fc46742c0cafbe78e30ce672f1eecb2e81df8c67
Pavel Kirienko <pavel.kirienko@gmail.com>
2020-12-08 18:23:12 +0200

    Fix path

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import sys
 import pathlib
 import importlib.util
 
-dll_path = pathlib.Path(importlib.util.find_spec('libpcap').origin).parent / '_platform' / '_windows' / ('x64' if sys.maxsize.bit_length() > 32 else 'x86')
+dll_path = pathlib.Path(importlib.util.find_spec('libpcap').origin).parent / '_platform' / '_windows' / ('x64' if sys.maxsize.bit_length() > 32 else 'x86') / 'wpcap'
 print('dll_path', dll_path)
 os.environ['PATH'] += os.pathsep + str(dll_path)
 
